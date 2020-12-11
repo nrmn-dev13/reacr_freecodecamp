a6617d06b351c1cbdc5d8682fc6da0017604677c
Norman Ramadhan <email: norman.ramadhan@bocistudio.com>
2020-12-11 10:58:25 +0700

    useState basic

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('Random Title');
+
+  const handleClick = () => {
+    if (text === ('Random Title')) {
+      setText('Hello World');
+    } else {
+      setText('Random Title');
+    }
+  }
+
+  return <>
+    <h1>{text}</h1>
+    <button className="btn" onClick={handleClick}>Change title</button>
+  </> ;
 };
 
 export default UseStateBasics;
