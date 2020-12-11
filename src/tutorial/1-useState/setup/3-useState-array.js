5de4d150c76d184c13b5b3f2371b20e93846618f
Norman Ramadhan <email: norman.ramadhan@bocistudio.com>
2020-12-11 14:38:09 +0700

    useState array example

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const handleButton = () => {
+    setPeople([]);
+  }
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  }
+
+  return (
+    <>
+      {people.map((person) => {
+        const {id, name} = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        )
+      })}
+      <button className="btn" onClick={() => handleButton()}>
+        clear items
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
